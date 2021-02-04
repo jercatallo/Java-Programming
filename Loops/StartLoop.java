8e3befc545016627db46d860147fb5dac4471a36
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2021-02-05 04:10:51 +0800

    javag

--- Loops/StartLoop.java
+++ Loops/StartLoop.java
@@ -1,17 +1,12 @@
 import java.util.Scanner;
 public class StartLoop {
-//     1. Write a program that asks the user for a starting value and an ending
-// value and then writes all the integers (inclusive) between those two values.
-// Enter Start:
-// 10
-// Enter End:
-// 15
+
     public static void main (String args[]){
         int sum=0;
 
-        Scanner sc= new Scanner (System.in);
-        System.out.println("Enter Start: "); int start=sc.nextInt();
-        System.out.println("Enter End: "); int end=sc.nextInt();
+        Scanner input= new Scanner (System.in);
+        System.out.println("Enter Start: "); int start=input.nextInt();
+        System.out.println("Enter End: "); int end=input.nextInt();
         System.out.print("\n");
         do{
             System.out.println(start);
